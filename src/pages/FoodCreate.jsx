62d1676a65fd2d367528ba29857001d61e9368fa
Henry Philpotts <hphilpotts@gmail.com>
2023-11-16 16:41:10 +0000

    Installed uuid and updated FoodCreate.jsx, added Stack from MUI

--- src/pages/FoodCreate.jsx
+++ src/pages/FoodCreate.jsx
@@ -1,4 +1,7 @@
 import React, { useContext, useState } from 'react'
+
+import { v4 as uuid } from 'uuid'
+
 import { FormControl } from '@mui/base'
 import { Button, Container, FormLabel, FormControlLabel, MenuItem, Radio, RadioGroup, Select, TextField } from '@mui/material'
 
@@ -8,6 +11,7 @@ import { convertEmoji as emoji } from '../utils/emojiHelpers'
 import { foodItemCategories, createFoodItem } from '../utils/foodHelpers'
 
 import foodEmojis from '../assets/data/emojiTree.json'
+import { Stack } from '@mui/system'
 
 export const FoodAdd = () => {
 
@@ -44,47 +48,49 @@ export const FoodAdd = () => {
   const currentIconObject = foodEmojis[iconGroup]
 
   const iconOptions = Object.keys(currentIconObject).map(item => (
-    <FormControlLabel value={item} key={item} control={<Radio />} label={emoji(currentIconObject[item])} />
+    <FormControlLabel value={item} key={uuid()} control={<Radio />} label={emoji(currentIconObject[item])} />
   ))
 
   return (
-    <Container>
-      <FormControl>
-        <FormLabel>Food Name</FormLabel>
-        <TextField id='food-name-text-field' name='name' onChange={changeHandler} value={formInput.name}></TextField>
-        <FormLabel>Category</FormLabel>
-        <Select
-          name='category'
-          value={formInput.category}
-          onChange={changeHandler}
-        >
-          {foodItemCategories.map((category, ix) => (
-            <MenuItem value={category} key={category + ix}>{category}</MenuItem>
-          ))}
-        </Select>
-        <FormLabel>Icon Category</FormLabel>
-        <Select
-          name='icon category'
-          value={iconGroup}
-          onChange={selectIconMenu}
-        >
-          {foodItemCategories.map((category, ix) => (
-            <MenuItem value={category} key={category + ix + 'b'}>{category}</MenuItem>
-          ))}
-        </Select>
+    <Container sx={{ display: 'flex', flexDirection: 'column' }}>
+      <FormControl sx={{ display: 'flex', flexDirection: 'row' }}>
+        <Stack>
+          <FormLabel>Food Name</FormLabel>
+          <TextField id='food-name-text-field' name='name' onChange={changeHandler} value={formInput.name}></TextField>
+          <FormLabel>Category</FormLabel>
+          <Select
+            name='category'
+            value={formInput.category}
+            onChange={changeHandler}
+          >
+            {foodItemCategories.map(category => (
+              <MenuItem value={category} key={uuid()}>{category}</MenuItem>
+            ))}
+          </Select>
+          <FormLabel>Icon Category</FormLabel>
+          <Select
+            name='icon category'
+            value={iconGroup}
+            onChange={selectIconMenu}
+          >
+            {foodItemCategories.map((category, ix) => (
+              <MenuItem value={category} key={category + ix + 'b'}>{category}</MenuItem>
+            ))}
+          </Select>
 
-        <FormLabel id="demo-radio-buttons-group-label">Icon</FormLabel>
-        <p>{iconGroup}</p>
-        <RadioGroup
-          aria-labelledby="demo-radio-buttons-group-label"
-          defaultValue="female"
-          name="radio-buttons-group"
-          row
-        >
-          {iconOptions}
-        </RadioGroup>
+          <FormLabel id="demo-radio-buttons-group-label">Icon</FormLabel>
+          <p>{iconGroup}</p>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="female"
+            name="radio-buttons-group"
+            row
+          >
+            {iconOptions}
+          </RadioGroup>
 
-        <Button type='submit' onClick={submitHandler}>submit</Button>
+          <Button type='submit' onClick={submitHandler}>submit</Button>
+        </Stack>
       </FormControl>
     </Container>
   )
