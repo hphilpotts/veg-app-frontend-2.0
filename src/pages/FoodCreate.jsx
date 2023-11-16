c4f82640ee462b5f7e61929f461c6f7788a5eead
Henry Philpotts <hphilpotts@gmail.com>
2023-11-16 16:09:08 +0000

    Emoji category selector added to form.

--- src/pages/FoodCreate.jsx
+++ src/pages/FoodCreate.jsx
@@ -17,10 +17,17 @@ export const FoodAdd = () => {
 
   const [formInput, setFormInput] = useState(emptyFormInput)
 
+  const [iconGroup, setIconGroup] = useState(formInput.category)
+
   const changeHandler = e => {
     const newInput = { ...formInput }
     newInput[e.target.name] = e.target.value
+    setIconGroup(newInput.category)
     setFormInput(newInput)
+  }
+
+  const selectIconMenu = e => {
+    setIconGroup(e.target.value)
   }
 
   const submitHandler = async () => {
@@ -37,8 +44,8 @@ export const FoodAdd = () => {
 
   return (
     <Container>
-      <p>{emoji(formInput.icon)}</p> {/* test */}
-      
+      {/* <p>{emoji(formInput.icon)}</p> test */}
+
       <FormControl>
         <FormLabel>Food Name</FormLabel>
         <TextField id='food-name-text-field' name='name' onChange={changeHandler} value={formInput.name}></TextField>
@@ -50,6 +57,16 @@ export const FoodAdd = () => {
         >
           {foodItemCategories.map((category, ix) => (
             <MenuItem value={category} key={category + ix}>{category}</MenuItem>
+          ))}
+        </Select>
+        <FormLabel>Icon Category</FormLabel>
+        <Select
+          name='icon category'
+          value={iconGroup}
+          onChange={selectIconMenu}
+        >
+          {foodItemCategories.map((category, ix) => (
+            <MenuItem value={category} key={category + ix + 'b'}>{category}</MenuItem>
           ))}
         </Select>
         <Button type='submit' onClick={submitHandler}>submit</Button>
