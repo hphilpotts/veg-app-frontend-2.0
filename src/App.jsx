1c733e1a92f1bc2a2f0b7885fc5ed504b953d9a8
Henry Philpotts <hphilpotts@gmail.com>
2023-12-04 17:15:38 +0000

    Layout changes to overall app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState, createContext } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
+import { Container } from '@mui/material';
+
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 
@@ -21,16 +23,18 @@ const App = () => {
   const signOut = () => { setUser(nullUser), navigateTo('signin/') };
 
   return (
-      <UserContext.Provider value={user}>
-        <Header signOut={signOut} />
+    <UserContext.Provider value={user}>
+      <Header signOut={signOut} />
+      <Container sx={{ display: 'flex', height: '100dvh', justifyContent: 'center', alignItems: 'center' }}>
         <Routes>
           <Route path='/' element={<p>Home</p>}></Route>
           <Route path='signin/' element={<SignIn setUserFromSignIn={setUserFromSignIn} />}></Route>
           <Route path='signup/' element={<SignUp setUserFromSignIn={setUserFromSignIn} />}></Route>
           <Route path='*' element={<p>no match</p>}></Route>
         </Routes>
-        <Footer />
-      </UserContext.Provider>
+      </Container>
+      <Footer />
+    </UserContext.Provider>
   );
 };
 
--- src/utils/muiTheme.js
+++ src/utils/muiTheme.js
@@ -1,7 +1,11 @@
 import { createTheme } from "@mui/material";
+import { green } from "@mui/material/colors";
 
 export const theme = createTheme({
     palette: {
+        primary : {
+            main: green[800]
+        },
         secondary: {
             main: '#FFFFFF'
         }
