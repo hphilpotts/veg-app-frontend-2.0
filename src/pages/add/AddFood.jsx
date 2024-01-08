191926a844eef190846237b412af848dd2290669
Henry Philpotts <hphilpotts@gmail.com>
2024-01-08 18:24:29 +0000

    Templated out revised AddFoods page.

--- src/pages/add/AddFood.jsx
+++ src/pages/add/AddFood.jsx
@@ -1,148 +1,49 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { v4 as uuid } from 'uuid';
+// main imports
+import React, { useContext } from 'react';
 
-import { Container, ButtonGroup, Button, Typography, Select, MenuItem, FormControl, FormHelperText, RadioGroup, FormControlLabel, Radio } from '@mui/material';
-import { flexColumnCentered as center, topMargin } from '../../utils/muiTheme';
+// component imports
+    // mui
+import { Stack, Container } from '@mui/material';
+    // other
 import { PageTitle } from '../../components/PageTitle';
-import { LoadingSkeleton } from '../../components/LoadingSkeleton';
-import { AddFoodButton } from './AddFoodButton';
 
-import { getFoods } from '../../utils/foodHelpers';
-import { subCategoriesWithEmojis as categoryData, subCategoriesWithDocumentKeys as keysForSubCategory } from '../../utils/foodCategories';
+// utils
+import { flexColumnCentered as center } from '../../utils/muiTheme';
 
+// other
 import { UserContext } from '../../App';
-import { useNavigate } from 'react-router-dom';
-
-// TODO - should probably break this out into separate files
-
-const mainCategoryOptions = Object.keys(categoryData); // ['veg', 'fruit', 'misc'] 
 
 export const AddFood = () => {
 
-    const defaultMainCategory = mainCategoryOptions[0] // 'veg'
-    const [mainCategory, setMainCategory] = useState(defaultMainCategory);
-
-    const defaultSubCategory = Object.keys(categoryData[defaultMainCategory])[0]; // top item in Select component within SubCategorySelector
-    const [subCategory, setSubCategory] = useState(defaultSubCategory);
-
-    const [foodsList, setFoodsList] = useState([]);
-
     const user = useContext(UserContext);
 
-    const setNewCategory = item => {
-        setMainCategory(item);
-        const newDefaultSubCategory = Object.keys(categoryData[item])[0]; // first sub category option for new main category
-        setSubCategory(newDefaultSubCategory);
-        populateFoodItems(newDefaultSubCategory);
-    };
-
-    const populateFoodItems = async selectedSubCategory => { // called on first render then subsequent changes to either main category state or sub category state
-        const categoryKey = keysForSubCategory[selectedSubCategory];
-        const response = await getFoods(user, categoryKey);
-        setFoodsList(response.Category);
-    };
-
-    const [selectedFood, setSelectedFood] = useState('');
-
-    const handleFoodSelect = event => {
-        setSelectedFood(event.target.value);
-    };
-
-    const navigateTo = useNavigate();
-
-    useEffect(() => {
-        if (!user.loggedIn) {
-            navigateTo('/');
-        } else {
-            populateFoodItems(subCategory);
-        };
-    }, [user]);
-
-    if (!user.loggedIn) {
-        return (<LoadingSkeleton />);
-    };
-
     return (
+        <Stack sx={{ height: '90vh', width: '100vw' }}>
+            <Container sx={{ height: '10%', ...center }}>
+                <PageTitle titleText={'log new foods'} />
+            </Container>
+            <Container sx={{ height: '10%', ...center }}>
+                <p>this is where the date selection bit goes</p>
+            </Container>
+            <Container sx={{ height: '10%', ...center }}>
+                <p>this will be the add new button</p>
+            </Container>
+            <Container sx={{ height: '70%', overflow: 'scroll' }}>
+                <p>this will be the already loaded data</p>
+                <p>this will be the already loaded data</p>
+                <p>this will be the already loaded data</p>
+                <p>this will be the already loaded data</p>
+                <p>this will be the already loaded data</p>
+                <p>this will be the already loaded data</p>
+                <p>this will be the already loaded data</p>
+                <p>this will be the already loaded data</p>
+                <p>this will be the already loaded data</p>
+                <p>this will be the already loaded data</p>
+                <p>this will be the already loaded data</p>
+                <p>this will be the already loaded data</p>
+                <p>this will be the already loaded data</p>
+            </Container>
+        </Stack>
+    )
 
-        // full page with main components below
-        <>
-            <PageTitle titleText={'log new foods'} />
-            <MainCategorySelector mainCategory={mainCategory} setMainCategory={setNewCategory} />
-            <SubCategorySelector mainCategory={mainCategory} subCategory={subCategory} setSubCategory={setSubCategory} populateFoodItems={populateFoodItems} />
-            <FoodItemSelector foodItemsList={foodsList} selectedFood={selectedFood} handleFoodSelect={handleFoodSelect} />
-            <AddFoodButton selectedFood={selectedFood} user={user} />
-        </>
-
-    );
-};
-
-
-const MainCategorySelector = ({ setMainCategory, mainCategory }) => {
-    return (
-
-        // select main category component
-        <Container sx={center}>
-            <ButtonGroup variant='contained' size='large'>
-                {mainCategoryOptions.map(item => {
-
-                    let colour = 'primary';
-                    if (item === mainCategory) colour = 'highlighted';
-
-                    return (
-                        <Button key={uuid()} color={colour}>
-                            <Typography variant='h6' onClick={() => setMainCategory(item)} sx={{ textTransform: 'lowercase' }}>{item}</Typography>
-                        </Button>
-                    );
-                })}
-            </ButtonGroup>
-        </Container>
-
-    );
-};
-
-
-const SubCategorySelector = ({ setSubCategory, mainCategory, subCategory, populateFoodItems }) => {
-
-    const mainCategoryObject = categoryData[mainCategory];
-
-    const handleChange = event => {
-        setSubCategory(event.target.value);
-        populateFoodItems(event.target.value);
-    };
-
-    return (
-
-        // select sub category component - options populated depending on main category selected above
-        <Container sx={{ ...center, ...topMargin }}>
-            <FormControl fullWidth sx={{ maxWidth: '300px', textTransform: 'lowercase' }}>
-                <FormHelperText>select a category</FormHelperText>
-                <Select
-                    value={subCategory}
-                    onChange={handleChange}
-                    displayEmpty
-                >
-                    {Object.keys(mainCategoryObject).map(key => (
-                        <MenuItem value={key} sx={{ textTransform: 'lowercase' }} key={uuid()} >{`${key} ${mainCategoryObject[key]}`}</MenuItem>
-                    ))}
-                </Select>
-            </FormControl>
-        </Container>
-
-    );
-};
-
-const FoodItemSelector = ({ foodItemsList, selectedFood, handleFoodSelect }) => {
-    if (!foodItemsList.length) {
-        return <LoadingSkeleton count={8} />
-    } else {
-        return (
-            <FormControl >
-                <RadioGroup name='food-item-selector-group' value={selectedFood} onChange={handleFoodSelect} >
-                    {foodItemsList.map(item => (
-                        <FormControlLabel value={item} control={<Radio />} label={item} key={uuid()} />
-                    ))}
-                </RadioGroup>
-            </FormControl>
-        );
-    };
-};
+}
