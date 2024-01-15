8f440b795020605896806753dd79da3a117cda75
Henry Philpotts <hphilpotts@gmail.com>
2024-01-15 11:24:47 +0000

    Added 'add' button to AddFoodButton.

--- src/components/AddFoodButton.jsx
+++ src/components/AddFoodButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { v4 as uuid } from 'uuid';
 
-import { Autocomplete, TextField, Container } from '@mui/material';
+import { Autocomplete, TextField, Container, Stack, Button, Typography } from '@mui/material';
 
 export const AddFoodButton = ({ foodsIndex, containerStyle }) => {
 
@@ -10,13 +10,20 @@ export const AddFoodButton = ({ foodsIndex, containerStyle }) => {
 
         return (
             <Container sx={containerStyle}>
-                <Autocomplete
-                    disablePortal
-                    options={foodsIndex}
-                    sx={{ width: 300 }}
-                    renderOption={(props, option) => (<li {...props} key={uuid()}>{option}</li>)}
-                    renderInput={(params) => <TextField {...params} label="search" />}
-                />
+                <Stack direction={'row'}>
+                    <Autocomplete
+                        disablePortal
+                        options={foodsIndex}
+                        sx={{ width: 275 }}
+                        renderOption={(props, option) => (<li {...props} key={uuid()}>{option}</li>)}
+                        renderInput={(params) => <TextField {...params} label="search" />}
+                    />
+                    <Container sx={{ width: '75px' }}>
+                        <Button key={uuid()} variant='contained' color='primary'>
+                            <Typography variant='h4' sx={{ textTransform: 'lowercase' }}>+</Typography>
+                        </Button>
+                    </Container>
+                </Stack>
             </Container>
 
         );
