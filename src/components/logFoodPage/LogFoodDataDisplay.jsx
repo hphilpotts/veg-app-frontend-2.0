ebd6f23960f4ee36224a36019b866da090bcddda
Henry Philpotts <hphilpotts@gmail.com>
2024-01-24 10:47:24 +0000

    Refactored LogFoodDataDisplay component.

--- src/components/logFoodPage/LogFoodDataDisplay.jsx
+++ src/components/logFoodPage/LogFoodDataDisplay.jsx
@@ -2,38 +2,31 @@ import React from 'react';
 
 import { v4 as uuid } from 'uuid';
 
-import { Container, IconButton } from '@mui/material';
+import { Container, IconButton, Typography } from '@mui/material';
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+
 
 export const LogFoodDataDisplay = ({ currentDayData, originalDayData }) => {
 
     let foodItemsList;
 
     if (currentDayData) {
-        foodItemsList = currentDayData.map(foodItem => {
-            if (originalDayData.includes(foodItem)) {
-                return <Container sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} key={uuid()}>
-                            <p>{foodItem}</p>
-                            <IconButton>
-                                <RemoveCircleOutlineIcon />
-                            </IconButton>
-                        </Container>
-            } else {
-                // todo - move key when refactoring
-                return <Container sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} key={uuid()}> 
-                            <p style={{ color: 'green' }} key={uuid()}>{foodItem}</p>
-                            <IconButton>
-                                <RemoveCircleOutlineIcon />
-                            </IconButton>
-                        </Container>
-            }
-
-        })
-    }
+        foodItemsList = currentDayData.map(foodItem => (
+            <Container sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }} key={uuid()}>
+                <Typography variant='body1' color={originalDayData.includes(foodItem) ? 'black': 'primary'} gutterBottom>
+                    {foodItem}
+                </Typography>
+                <IconButton>
+                    <RemoveCircleOutlineIcon />
+                </IconButton>
+            </Container>
+        ));
+    };
 
     return (
         <Container sx={{ height: '70%', overflow: 'scroll' }}>
             {foodItemsList ? foodItemsList.reverse() : null}
         </Container>
-    )
-}
+    );
+    
+};
