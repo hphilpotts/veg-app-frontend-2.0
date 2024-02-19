35b2510659ae6273dbb4c10e47d43547ebcb2317
Henry Philpotts <hphilpotts@gmail.com>
2024-02-19 10:05:25 +0000

    Progress page fetching Week data ok.

--- src/pages/Progress.jsx
+++ src/pages/Progress.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Axios from 'axios';
+import { xAuth } from '../utils/axiosConfig';
+
+import { UserContext } from '../App';
+
 
 export const Progress = () => {
+
+    const user = useContext(UserContext);
+
+    const [weekData, setWeekData] = useState({});
+
+    const fetchWeekData = async (user, date) => {
+
+        const formattedDate = date.toISOString().split('T')[0];
+        const requestUrl = `/api/week/find?user=${user.id}&date=${formattedDate}`;
+
+        try {
+            const res = await Axios.get(requestUrl, xAuth(user.token));
+            setWeekData(res.data.Week)
+        } catch (error) {
+            error.message.includes('Cannot read properties of null') ?
+                console.warn('the week document you are fetching does not exist!') :
+                console.error(error);
+        };
+
+    };
+
+    const navigateTo = useNavigate();
+
+    useEffect(() => {
+        if (!user.loggedIn) {
+            navigateTo('/');
+        } else {
+            fetchWeekData(user, new Date());
+        }
+    }, [user]);
+
     return (
         <h1>Progress Page</h1>
-    )
+    );
+
 };
