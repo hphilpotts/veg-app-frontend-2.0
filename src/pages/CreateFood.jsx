9c8418266ca6bfdb485d535fb551a373dea17191
Henry Philpotts <hphilpotts@gmail.com>
2024-02-25 11:34:41 +0000

    Structured CreateFood form.

--- src/pages/CreateFood.jsx
+++ src/pages/CreateFood.jsx
@@ -1,13 +1,24 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { v4 as uuid } from 'uuid';
+
+import { MenuItem, Stack, FormControl, TextField, InputLabel, Select, Button } from '@mui/material';
 import { PageTitle } from '../components/PageTitle';
+
+import { subCategoriesWithDocumentKeys as documentCategories } from '../utils/foodCategories';
 
 import { UserContext } from '../App';
 
 export const CreateFood = () => {
 
     const user = useContext(UserContext);
+
+    const [category, setCategory] = useState("");
+
+    const handleCategoryChange = e => {
+        setCategory(e.target.value);
+    };
 
     const navigateTo = useNavigate();
 
@@ -17,7 +28,26 @@ export const CreateFood = () => {
         };
     }, [user]);
 
+    const categoryMenuItems = Object.keys(documentCategories).map(key => (<MenuItem value={key} key={uuid()}>{key}</MenuItem>));
+
     return (
-        <PageTitle titleText={'create food'} />
+        <>
+            <PageTitle titleText={'create food'} />
+            <Stack spacing={1}>
+                <TextField label='food name' />
+                <FormControl>
+                    <InputLabel id="category-select-input-label">category</InputLabel>
+                    <Select
+                        value={category}
+                        onChange={handleCategoryChange}
+                        label="category"
+                        labelId="category-select-input-label">
+                        {categoryMenuItems}
+                    </Select>
+                </FormControl>
+                <Button variant='contained'>add new food</Button>
+                <Button variant='outlined'>add and log for today</Button>
+            </Stack>
+        </>
     );
 };
