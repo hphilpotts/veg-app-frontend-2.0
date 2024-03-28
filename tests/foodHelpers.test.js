dc09d976b89c811860236681ed0e9d97d3def847
Henry Philpotts <hphilpotts@gmail.com>
2024-03-28 11:19:51 +0000

    Added further unit tests for createNewFoodDocument.

--- tests/foodHelpers.test.js
+++ tests/foodHelpers.test.js
@@ -1,17 +1,48 @@
-import { describe, test, expect, beforeEach, vi } from "vitest";
+import { describe, test, expect, beforeEach, vi, afterEach } from "vitest";
 import * as exports from '../src/utils/foodHelpers';
-import Axios from "axios";
+import axios from "axios";
 
 describe("createNewFoodDocument should", () => {
+
+    const mockUser = { id: 'dummyUserId' };
+
+    const consoleErrorMock = vi.spyOn(console, 'error').mockImplementation(() => undefined);
 
     beforeEach(() => {
         vi.mock('axios');
     });
 
-    test("make an Axios POST request", () => {
+    afterEach(() => {
+        consoleErrorMock.mockReset();
+    });
+
+    test("make an Axios POST request as expected given the arguments passed in", () => {
         exports.createNewFoodDocument({ id: 'dummyUserId' });
-        expect(Axios.post).toHaveBeenCalledOnce();
-        expect(Axios.post).toHaveBeenCalledWith('/api/foods/create', { user: 'dummyUserId' }, { headers: { 'x-auth-token': undefined } });
+        expect(axios.post).toHaveBeenCalledOnce();
+        expect(axios.post).toHaveBeenCalledWith('/api/foods/create', { user: 'dummyUserId' }, { headers: { 'x-auth-token': undefined } });
+    });
+
+    test("return a respose as expected if successful", async () => {
+        axios.post.mockResolvedValueOnce({ res: { status: 201 } });
+        const response = await exports.createNewFoodDocument(mockUser);
+        expect(response).toBeTypeOf('object');
+        expect(response).toHaveProperty('res');
+        expect(response.res).toHaveProperty('status');
+        expect(response.res.status).toBe(201);
+    })
+
+    test("log an error to the console if unsuccessful", async () => {
+        axios.post.mockRejectedValueOnce(new Error("Error creating new Foods document, please try again later"));
+        await exports.createNewFoodDocument(mockUser);
+        expect(consoleErrorMock).toHaveBeenCalledOnce();
+    });
+
+    test("return an error with expected message if unsuccessful", async () => {
+        axios.post.mockRejectedValueOnce(new Error("Error creating new Foods document, please try again later"));
+        const response = await exports.createNewFoodDocument(mockUser);
+        expect(response).toBeTypeOf('object');
+        expect(response).toBeInstanceOf(Error);
+        expect(response.message).toBe("Error creating new Foods document, please try again later");
     });
 
 });
